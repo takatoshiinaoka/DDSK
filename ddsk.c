feeece3b724315600129e76e6889ed7bf8ac8e67
takatoshiinaoka <heavenhayao2316@gmail.com>
2022-08-08 22:08:24 +0900

    [add] count

---
+++ ddsk.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char *ddsk[2] = {"ドド","スコ"};
+    char *history[12] = {""};
+    int count = 1;
+
+    while(1){
+        for (int i=11; i>0; i--)
+            history[i] = history[i-1];
+        
+        history[0] = ddsk[rand() % 2];
+        
+        if(history[0]==ddsk[1] && history[1]==ddsk[1] && history[2]==ddsk[1] && history[3]==ddsk[0]
+        && history[4]==ddsk[1] && history[5]==ddsk[1] && history[6]==ddsk[1] && history[7]==ddsk[0]
+        && history[8]==ddsk[1] && history[9]==ddsk[1] && history[10]==ddsk[1] && history[11]==ddsk[0])
+            break;
+        
+        printf("%s\n",history[11]);
+        count += 1;
+    }
+
+    for(int i=11; i>=0; i--)
+        i % 4 == 0 ? printf("%s ", history[i]) : printf("%s", history[i]);
+    
+    printf("ラブ注入❤︎\ncount : %d", count);
+
+    return 0;
+}
